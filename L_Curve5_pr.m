b5cb63f11af8b5eb59d50c14d6770eeb82a77eb7
Albertl <lalbert@email.arizona.edu>
2022-10-29 21:28:03 -0400

    Create L_Curve5_pr.m

---
+++ L_Curve5_pr.m
@@ -0,0 +1,17 @@
+function P_Predict=L_Curve5_pr(var, P, I)
+%% P is photosynthesis
+%% I is Light intensity, PAR
+%% var is varible for the model
+
+%% L_Curve5-- Prioul and Chartier 1977, Leverenz 1987, 1988, and Levernez et al., 1990
+
+Pm=var(1);
+alfa=var(2);
+Rd=var(3);
+theta=var(4);
+
+P_Predict=alfa.*I+Pm-sqrt((alfa.*I+Pm).^2-4.*alfa.*I.*Pm.*theta);
+P_Predict=P_Predict/(2*theta)+Rd;
+
+f=P-P_Predict;
+df=sqrt(sum(f.^2)./length(f));
