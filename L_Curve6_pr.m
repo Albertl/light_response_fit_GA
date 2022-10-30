9a293a281dfbf8a3e08b7cba1284f4325418f61b
Albertl <lalbert@email.arizona.edu>
2022-10-29 21:28:08 -0400

    Create L_Curve6_pr.m

---
+++ L_Curve6_pr.m
@@ -0,0 +1,18 @@
+function P_Predict=L_Curve6_pr(var, P, I)
+%% P is photosynthesis
+%% I is Light intensity, PAR
+%% var is varible for the model
+
+%% L_Curve6-- Chalker 1980
+
+Pm=var(1);
+alfa=var(2);
+Rd=var(3);
+theta=var(4);
+
+x1=exp(alfa.*I.*(1+theta));
+
+P_Predict=Pm.*((x1-1)./(x1+theta))+Rd;
+
+f=P-P_Predict;
+df=sqrt(sum(f.^2)./length(f));
