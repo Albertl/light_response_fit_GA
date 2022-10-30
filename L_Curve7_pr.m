27ffbb266f48fdccc753dd18fe7542705ac1c431
Albertl <lalbert@email.arizona.edu>
2022-10-29 21:28:13 -0400

    Create L_Curve7_pr.m

---
+++ L_Curve7_pr.m
@@ -0,0 +1,16 @@
+function P_Predict=L_Curve7_pr(var, P, I)
+%% P is photosynthesis
+%% I is Light intensity, PAR
+%% var is varible for the model
+
+%% L_Curve7-- Bannister 1979
+
+Pm=var(1);
+alfa=var(2);
+Rd=var(3);
+theta=var(4);
+
+P_Predict=Pm.*(alfa.*I)./((Pm.^theta+(alfa.*I).^theta).^(1/theta))+Rd;
+
+f=P-P_Predict;
+df=sqrt(sum(f.^2)./length(f));
