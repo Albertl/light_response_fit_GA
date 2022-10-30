f2b3be85d2936b25ba003cbdec53fe780fb71484
Albertl <lalbert@email.arizona.edu>
2022-10-29 21:27:40 -0400

    Create L_Curve1.m

---
+++ L_Curve1.m
@@ -0,0 +1,15 @@
+function df=L_Curve1(var, P, I)
+%% P is photosynthesis
+%% I is Light intensity, PAR
+%% var is varible for the model
+
+%% L_Curve1-- Baly 1935 rectanguar Hyerbola
+
+Pm=var(1);
+alfa=var(2);
+Rd=var(3);
+
+P_Predict=Pm.*(alfa.*I./(Pm+alfa.*I))+Rd;
+
+f=P-P_Predict;
+df=sqrt(sum(f.^2)./length(f));
